78a8ae96445948ecc9ac9400f8a6577e997c82a7
twalla26 <ssm4748@naver.com>
2026-03-13 22:17:04 +0900

    week17/같은 숫자는 싫어/twalla26

---
+++ src/week17/PGMS12906/twalla26/Solution.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < arr.length; i++) {
+            
+            if (stack.isEmpty()) {
+                stack.push(arr[i]);
+                continue;
+            }
+            
+            if (stack.peek() == arr[i]) {
+                continue;
+            }
+            
+            stack.push(arr[i]);
+            
+        }
+        
+        int[] answer = new int[stack.size()];
+        int idx = stack.size() - 1;
+        while (!stack.isEmpty()) {
+            answer[idx] = stack.pop();
+            idx -= 1;
+        }
+
+        return answer;
+    }
+}
